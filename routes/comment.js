9d7f81af2ad22a5f8bba550e4af94c02d388abf4
Moonhee <o119a@naver.com>
2022-04-12 16:03:07 +0900

    updated comment

--- routes/comment.js
+++ routes/comment.js
@@ -5,10 +5,9 @@ const router = express.Router();
 
 // Comment조회: 클라이언트에 보내기
 router.get('/:user/comments', async (req, res) => {
-
     let userId = req.params.user
     const comment = await Comments.find({userId}).exec();
-    // console.log(comment)
+    console.log(comment)
     res.json(comment);
     
 });
@@ -16,24 +15,17 @@ router.get('/:user/comments', async (req, res) => {
 // Comment추가
 router.post('/:user/comments', async (req, res) => {
     const{ writer, content, Date, userId} = req.body
-
+ 
     await Comments.create({writer, content, Date, userId})
     return res.status(200).send({ });
 });
 
 
-// Comment삭제 (심심해서..테스트..)
+// Comment삭제 
 router.delete('/:user/comments', async (req, res) => {
     const{ commentId, userId} = req.body
-    console.log({commentId})
-    console.log(req.body,'<<<<<')
-    const comment = await Comments.findOne({_id: commentId}).exec();
-    const theOne = await Comments.find({userId}).exec();
 
-    console.log(comment._id)
-    console.log(theOne)
-
-    // await Comments.delete({writer, content, Date, userId})
+    await Comments.deleteOne({ commentId });
     return res.status(200).send({msg: '완료' });
 });
 
---
+++ schemas/comments.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    writer: String,
+    content: String,
+    Date: String,
+    userId: String,
+    // 수정된 부분: date: date, userId: userID로 된 부분 value 값 string으로 수정
+});
+
+commentSchema.virtual("commentId").get(function () {
+     return this._id.toHexString(); 
+}); 
+commentSchema.set("toJSON", {
+     virtuals: true, 
+});
+
+
+module.exports = mongoose.model('Comments', commentSchema);
